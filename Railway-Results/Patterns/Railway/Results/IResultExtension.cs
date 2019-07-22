509f52b97848adb056e1ee5d9dab90181623fbc3
DESKTOP-JEPURJ7\koern <koerner.jena@web.de>
2019-07-22 20:11:36 +0200

    Documentes extension methods and updated Assembly version to 1.1.0.1.

--- Railway-Results/Patterns/Railway/Results/IResultExtension.cs
+++ Railway-Results/Patterns/Railway/Results/IResultExtension.cs
@@ -7,6 +7,14 @@ namespace KoeLib.Patterns.Railway.Results
     [DebuggerStepThrough]
     public static class IResultExtension
     {
+        /// <summary>
+        /// Allways calls the <paramref name="action"/>.
+        /// </summary>
+        /// <typeparam name="TResult">The type of This Result.</typeparam>
+        /// <param name="target">This Result.</param>
+        /// <param name="action">The Action.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public static TResult Then<TResult>(this TResult target, Action action)
            where TResult : IResult
         {
@@ -15,6 +23,15 @@ namespace KoeLib.Patterns.Railway.Results
             return target;
         }
 
+        /// <summary>
+        /// Allways calls and returns the <typeparamref name="TNewResult"/> of <paramref name="nextResult"/>.
+        /// </summary>
+        /// <typeparam name="TResult">The type of This Result.</typeparam>
+        /// <typeparam name="TNewResult">The type of the new Result thats returned.</typeparam>
+        /// <param name="_">This Result.</param>
+        /// <param name="nextResult">The function that returns the <typeparamref name="TNewResult"/>.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public static TNewResult Then<TResult, TNewResult>(this TResult _, Func<TNewResult> nextResult)
            where TResult : IResult
            where TNewResult : IResult
@@ -23,16 +40,33 @@ namespace KoeLib.Patterns.Railway.Results
             return nextResult();
         }
 
-
-        public static TNewResult Then<TResult, TNewResult>(this TResult target, Func<TResult, TNewResult> func)
+        /// <summary>
+        /// Allways calls and returns the <typeparamref name="TNewResult"/> of <paramref name="newResultFunc"/>.
+        /// </summary>
+        /// <typeparam name="TResult">The type of This Result.</typeparam>
+        /// <typeparam name="TNewResult">The type of the new Result thats returned.</typeparam>
+        /// <param name="target">This Result.</param>
+        /// <param name="newResultFunc">The function that returns the <typeparamref name="TNewResult"/>.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static TNewResult Then<TResult, TNewResult>(this TResult target, Func<TResult, TNewResult> newResultFunc)
             where TResult : IResult
             where TNewResult : IResult
         {
-            Args.ExceptionIfNull(func, nameof(func));
-            return func(target);
+            Args.ExceptionIfNull(newResultFunc, nameof(newResultFunc));
+            return newResultFunc(target);
         }
 
-
+        /// <summary>
+        /// Calls <paramref name="keepFunc"/> and stores the <typeparamref name="T"/> in <paramref name="kept"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the kept value.</typeparam>
+        /// <typeparam name="TResult">This Result.</typeparam>
+        /// <param name="target">This Result.</param>
+        /// <param name="keepFunc">The funtion that provides the <typeparamref name="T"/> to keep in <paramref name="kept"/>.</param>
+        /// <param name="kept">The kept value.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public static TResult Keep<T, TResult>(this TResult target, Func<T> keepFunc, out T kept)
             where TResult: IResult
         {
@@ -41,6 +75,15 @@ namespace KoeLib.Patterns.Railway.Results
             return target;
         }
 
+        /// <summary>
+        /// Returns a <see cref="TryCatchResult{T}"/> that tries to call <paramref name="resultFunc"/> in a <see cref="TryCatchResult{T}.Catch(Func{Exception, T})"/>.
+        /// </summary>
+        /// <typeparam name="TResult">This result.</typeparam>
+        /// <typeparam name="T">The return value of the <see cref="TryCatchResult{T}.Catch(Func{Exception, T})"/>.</typeparam>
+        /// <param name="result">This Result.</param>
+        /// <param name="resultFunc">The function thats being called in a <see cref="TryCatchResult{T}.Catch(Func{Exception, T})"/></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public static TryCatchResult<T> Try<TResult, T>(this TResult result, Func<TResult, T> resultFunc)
            where TResult : IResult
         {
